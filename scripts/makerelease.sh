04735c6d92ed9edec283b3e796050d5928c53cce
André Offringa <offringa@gmail.com>
2012-11-10 23:44:50 +1100

    Document work tree attrs

--- scripts/makerelease.sh
+++ scripts/makerelease.sh
@@ -5,6 +5,7 @@ else
     VERSION="$1"
     curdir=`pwd`
     cd ${curdir%/scripts}
+# To use current work tree attributes: "--worktree-attributes"
     git archive --format=tar --prefix=aoflagger-${VERSION}/ master | bzip2 -9 > ${curdir}/aoflagger-${VERSION}.tar.bz2 &&
     echo Wrote ${curdir}/aoflagger-${VERSION}.tar.bz2
 fi
