98cbb61919c872448f0e5d9b61e3f01040e3f688
André Offringa <offringa@gmail.com>
2012-12-03 00:32:34 +1100

    More work on strategy wizard

--- src/gui/strategywizardwindow.h
+++ src/gui/strategywizardwindow.h
@@ -2,9 +2,31 @@
 #define STRATEGY_WIZARD_WINDOW_H
 
 #include <gtkmm/window.h>
+#include <gtkmm/notebook.h>
+#include <gtkmm/box.h>
+#include <gtkmm/button.h>
+#include <gtkmm/buttonbox.h>
+#include <gtkmm/combo.h>
 
 class StrategyWizardWindow : public Gtk::Window
 {
+public:
+	StrategyWizardWindow();
+private:
+	Gtk::Notebook _notebook;
+	Gtk::VBox _mainBox, _telescopeBox, _optionsBox;
+	Gtk::HBox _telescopeSubBox;
+	Gtk::Label _telescopeLabel;
+	Gtk::Combo _telescopeCombo;
+	Gtk::HButtonBox _buttonBox;
+	Gtk::Button _finishButton, _nextButton, _previousButton;
+	Gtk::CheckButton _guiOptimizationButton;
+	
+	void onNextClicked();
+	void onPreviousClicked();
+	void onFinishClicked();
+	void onPageSwitched(GtkNotebookPage *page, guint pageNumber);
+	void updateSensitivities();
 };
 
 #endif
